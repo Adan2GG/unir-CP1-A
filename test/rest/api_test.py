c8d10ff55bd97ed4e5b6f487a0fc3e1dbbf1b5be
anieto UNIR <anieto.unir@localhost.com>
2021-11-14 19:50:19 +0100

    test api extended sqrt

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -5,7 +5,8 @@ from urllib.request import urlopen
 
 import pytest
 
-BASE_URL = "http://localhost:5000" #os.environ.get("BASE_URL")
+BASE_URL = "http://localhost:5000"
+BASE_URL_MOCK = "http://localhost:9090"
 DEFAULT_TIMEOUT = 2  # in secs
 
 @pytest.mark.api
@@ -21,7 +22,17 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "3", "ERRORRRRR"
+            response.read().decode(), "3", "ERROR ADD"
+        )
+
+    def test_api_sqrt(self):
+        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "8", "ERROR SQRT"
         )
 
 if __name__ == "__main__":  # pragma: no cover
