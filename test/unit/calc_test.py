dbeddcd9a68324c5bf0b99129765e5f4d421f432
anieto-unir <94193177+anieto-unir@users.noreply.github.com>
2022-05-26 03:00:15 +0200

    Restore Div0 Test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -40,11 +40,11 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
-#    def test_divide_method_fails_with_division_by_zero(self):
-#        self.assertRaises(TypeError, self.calc.divide, 2, 0)
-#        self.assertRaises(TypeError, self.calc.divide, 2, -0)
-#        self.assertRaises(TypeError, self.calc.divide, 0, 0)
-#        self.assertRaises(TypeError, self.calc.divide, "0", 0)
+    def test_divide_method_fails_with_division_by_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+        self.assertRaises(TypeError, self.calc.divide, 2, -0)
+        self.assertRaises(TypeError, self.calc.divide, 0, 0)
+        self.assertRaises(TypeError, self.calc.divide, "0", 0)
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
