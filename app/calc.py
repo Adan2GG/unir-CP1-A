f6a5adea27ec93415fc9453bb69c738629684c8c
anieto-unir <94193177+anieto-unir@users.noreply.github.com>
2023-01-18 18:35:39 +0100

    Fix errors

--- app/calc.py
+++ app/calc.py
@@ -11,11 +11,11 @@ class Calculator:
         return x + y
 
     def substract(self, x, y):
-        self.                                                                                                         check_types(x, y)
+        self.check_types(x, y)
         return x - y
 
     def multiply(self, x, y):
-        self.                                                                                                      check_types(x, y)
+        self.check_types(x, y)
         return x * y
 
     def divide(self, x, y):
