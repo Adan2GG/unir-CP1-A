e4c5784b98c08bb6e6c4df4dd4870f4675092809
anieto-unir <94193177+anieto-unir@users.noreply.github.com>
2023-12-19 19:38:31 +0100

    Blank spaces error fixed

--- app/api.py
+++ app/api.py
@@ -18,7 +18,7 @@ def hello():
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
     try:
-        num_1, num_2 =            util.convert_to_number(op_1), util.convert_to_number(op_2)
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
         return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
