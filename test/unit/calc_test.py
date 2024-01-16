1ffbf0c8513aca213d028750438f8d0916f5cb50
anieto-unir <94193177+anieto-unir@users.noreply.github.com>
2024-01-16 03:34:35 +0100

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -25,12 +25,7 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
-
-    def test_divide_method_fails_by_zero(self):
-        self.assertRaises(TypeError, self.calc.divide, 4, 0)
-        self.assertRaises(TypeError, self.calc.divide, 0, 0)
-        self.assertRaises(TypeError, self.calc.divide, -0, -0)
-    
+  
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
