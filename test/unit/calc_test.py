d90c709cec130afa4078786e4bf821473d9e7af2
Adan Garcia <adan2gg@hotmail.com>
2024-04-29 14:07:42 +0200

    Modificacion test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -10,7 +10,7 @@ class TestCalculate(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add_method_returns_correct_result(self):
-        self.assertEqual(5, self.calc.add(2, 2))
+        self.assertEqual(4, self.calc.add(2, 2))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))
