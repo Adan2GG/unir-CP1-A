fc80938680221a645b4f5665b9083708478cdd3c
Adan2GG <167446338+Adan2GG@users.noreply.github.com>
2024-05-03 09:38:40 +0200

    Update api_test.py multiply y divide

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -34,6 +34,24 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+        def test_api_multiply(self)
+        url = f"{BASE_URL}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4", "ERROR MULTIPLY"
+        )
+    def test_api_divide(self)
+        url = f"{BASE_URL}/calc/divide/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2", "ERROR DIVIDE"
+        )
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
